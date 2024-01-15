025d9f25be1dafb2f9c916ef16114e59b15c1754
o-su <xmbvztcxzc@email.cz>
2024-01-15 15:28:03 +0100

    added favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className} style={{ height: "100%" }}>
         <Providers>
           <AppLayout header={<AppHeader />}>{children}</AppLayout>
